a7037b1c4b12713ddb1fc8fb5878c753a879d918
hansraj2003 <hansjoshi2003no1@gmail.com>
2024-02-07 23:38:19 +0530

    react

--- 06currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06currencyConverter/src/hooks/useCurrencyInfo.js
@@ -7,9 +7,13 @@ function useCurrencyInfo(currency) {
         )
         .then((res) => res.json())
         .then((res) => setData(res[currency]))
+        
         console.log(data);  
+
     },[currency])
     console.log(data);  
     
     return data
 }
+
+export default useCurrencyInfo;
