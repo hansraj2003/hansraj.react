94b0558a9119f6141395c86d6d20e723ebad3d8a
hansraj2003 <hansjoshi2003no1@gmail.com>
2024-02-10 13:04:15 +0530

    react

---
+++ 07reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+
+function Github() {
+    const data = useLoaderData();
+    // const [data, setData] =useState([]);
+    // useEffect(() =>{
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => 
+    //        { console.log(data);
+    //         setData(data)}
+    //         )
+    // },[Github])
+    return ( 
+        <div className='text-center m-4 bg-gray-600 text-white text-3xl p-4'>
+            Github Followers: {data.followers}
+            <img src={data.avatar_url} alt="Git profile pic" width={300}/>
+        </div>
+     );
+}
+
+export default Github;
+
+export const githubInfoLoader = async function() {
+   const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json();
+}
---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { Route, RouterProvider, Routes, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import About from './components/About/About.jsx'
+import Contact from './components/Contact/Contact.jsx'
+import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Layout/>,
+//     children: [
+//       {
+//         path: '',
+//         element: <Home/>
+//       },
+//       {
+//         path: '',
+//         element: <Header/>
+//       },
+//       {
+//         path: "/contact",
+//         element: <Contact/>
+//       },
+//       {
+//         path: 'about',
+//         element: <About/>
+//       }
+//     ],
+//   }
+// ])
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route path='' element={<Home />} />
+      {/* <Route path='' element={<Header/>}/> iska route nahi denge tab bhi chalega kyuki header layout.jsx folder me included hai*/}
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userid' element={<User />} />
+      <Route
+        loader={githubInfoLoader}
+        path='github'
+        element={<Github />} />
+
+    </Route>
+  )
+
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
