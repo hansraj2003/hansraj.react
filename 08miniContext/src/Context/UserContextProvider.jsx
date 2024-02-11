dffd42a5295d2f0fb9a6181d2dbb16f716b7639d
hansraj2003 <hansjoshi2003no1@gmail.com>
2024-02-11 21:11:57 +0530

    react

--- 08miniContext/src/Context/UserContextProvider.jsx
+++ 08miniContext/src/Context/UserContextProvider.jsx
@@ -3,11 +3,14 @@ import UserContext from './UserContext';
 
 const UserContextProvider = ({children}) => {
     const [user, setUser] = useState(null)
+    console.log({user, setUser});
+    console.log({children});
     return (
         <UserContext.Provider value={{user, setUser}}>
          {children }
         </UserContext.Provider>
     )
+    
 }
 
 export default UserContextProvider;
