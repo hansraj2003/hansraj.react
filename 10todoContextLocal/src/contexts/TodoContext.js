f2923ead6eaffe890de672bf7e2cdcf7c07cb942
hansraj2003 <hansjoshi2003no1@gmail.com>
2024-02-16 22:40:22 +0530

    react

---
+++ 10todoContextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,22 @@
+import React, { createContext, useContext } from 'react';
+
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Todo msg",
+            completed: false,
+        }
+    ], 
+    addTodo: (todo) => {},
+    updateTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {},
+
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
